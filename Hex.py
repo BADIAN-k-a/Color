5c009be8dc563bea2fddb4b99e606e29650f7acd
BADIAN-k-a <145933570+BADIAN-k-a@users.noreply.github.com>
2023-10-28 04:50:33 +0500

    Add files via upload

---
+++ Hex.py
@@ -0,0 +1,11 @@
+import re
+
+def is_valid_hex_color(color):
+    pattern = r'^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$'
+    return bool(re.match(pattern,color))
+
+color1="#AAbc19"
+color2="#3f3"
+color3="#Zad43a"
+
+print(is_valid_hex_color(color3))
